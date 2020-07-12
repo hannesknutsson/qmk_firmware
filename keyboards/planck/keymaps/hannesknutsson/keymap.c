a8710be3b6f3cdf1515b3263ba9ce83e35c22037
Hanke <hannes.knutsson96@gmail.com>
2020-07-12 21:50:16 +0200

    Add missing special characters to lower layer and fix minor mistakes

--- keyboards/planck/keymaps/hannesknutsson/keymap.c
+++ keyboards/planck/keymaps/hannesknutsson/keymap.c
@@ -21,8 +21,7 @@ enum planck_layers {
   _BASE,
   _GAME,
   _LOWER,
-  _RAISE,
-  _FN
+  _RAISE
 };
 
 #define LOWER 		MO(_LOWER)		//Momentarily change to lower layer
@@ -45,8 +44,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 	//Game layer. Shifts top three rows down one row to allow for a persistent number row.
 	[_GAME] = LAYOUT_planck_grid(
-	    KC_ESC ,  SE_1, SE_2, SE_3, SE_4, SE_5  , SE_6  , SE_7, SE_8   , SE_9  , SE_0   , SE_ARNG   ,
-	    KC_TAB ,  SE_Q, SE_W, SE_E, SE_R, SE_T  , SE_Y  , SE_U, SE_I   , SE_O  , SE_P   , SE_ADIA   ,
+	    KC_ESC ,  SE_1, SE_2, SE_3, SE_4, SE_5  , SE_6  , SE_7, SE_8   , SE_9  , SE_0   , SE_PLUS   ,
+	    KC_TAB ,  SE_Q, SE_W, SE_E, SE_R, SE_T  , SE_Y  , SE_U, SE_I   , SE_O  , SE_P   , SE_ARNG   ,
 	    KC_LSFT,  SE_A, SE_S, SE_D, SE_F, SE_G  , SE_H  , SE_J, SE_K   , SE_L  , SE_ODIA, MT_RSH_ENT,
 	    KC_LCTL,  SE_Z, SE_X, SE_C, SE_V, KC_SPC, KC_SPC, SE_M, SE_COMM, SE_DOT, SE_MINS, DEF_BASE
 	),
@@ -54,15 +53,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	//Raise layer containing special characters and arrow keys.
 	[_RAISE] = LAYOUT_planck_grid(
 	    _______, _______, _______, SE_LPRN, SE_RPRN, _______, _______, _______, _______, _______ , _______, KC_DEL ,
-	    _______, SE_PIPE, _______, SE_LCBR, SE_RCBR, _______, KC_LEFT, KC_DOWN, KC_UP  , KC_RIGHT, SE_TILD, KC_BSPC,
-	    _______, SE_LABK, SE_RABK, SE_LBRC, SE_RBRC, _______, _______, _______, _______, _______ , SE_ASTR, _______,
+	    _______, SE_PIPE, _______, SE_LCBR, SE_RCBR, _______, KC_LEFT, KC_DOWN, KC_UP  , KC_RIGHT, _______, KC_BSPC,
+	    _______, SE_LABK, SE_RABK, SE_LBRC, SE_RBRC, _______, _______, _______, _______, _______ , _______, _______,
 	    _______, _______, _______, _______, XXXXXXX, _______, _______, _______, _______, _______ , _______, _______
 	),
 
 	//Lower layer containing numbers, F-keys, backspace and delete.
 	[_LOWER] = LAYOUT_planck_grid(
-	    _______, SE_1   , SE_2   , SE_3   , SE_4   , SE_5   , SE_6   , SE_7  , SE_8   , SE_9   , SE_0   , KC_DEL ,
-	    _______, KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , SE_4  , SE_5   , SE_6   , SE_CIRC, KC_BSPC,
+	    _______, SE_1   , SE_2   , SE_3   , SE_4   , SE_5   , SE_6   , SE_7  , SE_8   , SE_9   , SE_0   , SE_PLUS,
+	    _______, KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , SE_4  , SE_5   , SE_6   , SE_DIAE, SE_ACUT,
 	    _______, KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 , SE_1  , SE_2   , SE_3   , SE_QUOT, _______,
 	    _______, _______, _______, _______, _______, _______, _______, SE_DOT, _______, _______, _______, _______
 	)
